88ba0b7e722009b1b55881d76e3addba491df30f
Mandizvidzafn <ncebamandizvidza@gmail.com>
2023-03-26 08:26:33 +0200

    DBStorage fixed State class and BaseModel instance

--- models/state.py
+++ models/state.py
@@ -1,30 +1,29 @@
 #!/usr/bin/python3
-""" State Module for HBNB project """
+"""This is the state class"""
+from sqlalchemy.orm import relationship
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
-from sqlalchemy.orm import relationship, backref 
+from models.city import City
 import models
+import os
 
 
 class State(BaseModel, Base):
-    """
-    State class inherits from BaseModel and Base
+    """This is the class for State
+    Attributes:
+        name: input name
     """
     __tablename__ = "states"
     name = Column(String(128), nullable=False)
 
-    if models.storage_t == "db":
-        cities = relationship("City", backref="state", cascade="all, delete")
-
-    @property
-    def cities(self):
-        """
-        Getter attribute that returns the list of City instances with state_id
-        equals to the current State.id
-        """
-        from models.city import City
-        city_list = []
-        for city in models.storage.all(City).values():
-            if city.state_id == self.id:
-                city_list.append(city)
-        return city_list
+    if os.getenv("HBNB_TYPE_STORAGE") == "db":
+        cities = relationship("City", cascade="all, delete", backref="state")
+    else:
+        @property
+        def cities(self):
+            """Return list of City instances with state_id equal to current State.id"""
+            city_list = []
+            for city in models.storage.all(City).values():
+                if city.state_id == self.id:
+                    city_list.append(city)
+            return city_list
