9ea655e58b3c8781aadc8980d0fca7a952e086e3
Mandizvidzafn <ncebamandizvidza@gmail.com>
2023-03-26 08:38:10 +0200

    fixed and BaseModel instance

--- models/base_model.py
+++ models/base_model.py
@@ -4,6 +4,7 @@ import uuid
 from datetime import datetime
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, String, Integer, DateTime
+from models import storage
 
 Base = declarative_base()
 
@@ -16,20 +17,23 @@ class BaseModel:
     updated_at = Column(DateTime, nullable=False, default=datetime.utcnow())
     
     def __init__(self, *args, **kwargs):
-        """Initialize a new BaseModel"""
-
-        if not kwargs.get('id'):
-            self.id = str(uuid.uuid4())
+        """Instatntiates a new model"""
+        if kwargs:
+            for k, v in kwargs.items():
+                if k == "created_at" or k == "updated_at":
+                    v = datetime.strptime(v, '%Y-%m-%dT%H:%M:%S.%f')
+                if k != "__class__":
+                    setattr(self, k, v)
+            if "id" not in kwargs.keys():
+                self.id = str(uuid.uuid4())
+            time = datetime.utcnow()
+            if "created_at" not in kwargs.keys():
+                self.created_at = time
+            if "updated_at" not in kwargs.keys():
+                self.updated_at = time
         else:
-            self.id = kwargs['id']
-
-        self.created_at = kwargs['created_at'] = datetime.utcnow() \
-            if 'created_at' not in kwargs else kwargs['created_at']
-        self.updated_at = kwargs['updated_at'] = datetime.utcnow() \
-            if 'updated_at' not in kwargs else kwargs['updated_at']
-
-        for key, value in kwargs.items():
-            setattr(self, key, value)
+            self.id = str(uuid.uuid4())
+            self.created_at = self.updated_at = datetime.utcnow()
 
 
     def __str__(self):
@@ -39,7 +43,7 @@ class BaseModel:
 
     def save(self):
         """Updates updated_at with current time when instance is changed"""
-        from models import storage
+        #from models import storage
         self.updated_at = datetime.now()
         storage.new(self)
         storage.save()
@@ -48,7 +52,8 @@ class BaseModel:
         """Return a dictionary representation of the instance"""
 
         dictionary = self.__dict__.copy()
-        dictionary.pop('_sa_instance_state', None)
+        if "_sa_instance_state" in dictionary:
+            dictionary.pop('_sa_instance_state', None)
         dictionary['created_at'] = self.created_at.isoformat()
         dictionary['updated_at'] = self.updated_at.isoformat()
         dictionary['__class__'] = type(self).__name__
@@ -57,5 +62,5 @@ class BaseModel:
     def delete(self):
         """Delete the current instance from the storage"""
 
-        from models import storage
+        #from models import storage
         storage.delete(self)
