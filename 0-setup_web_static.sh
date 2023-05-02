0d2582c67f2738c5b80b1ef05fff90dc779ac1c2
Mandizvidzafn <ncebamandizvidza@gmail.com>
2023-05-02 18:16:35 +0200

    web static

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,12 +1,26 @@
 #!/usr/bin/env bash
 # sets up the web servers for the deployment of web_static
 
-sudo apt-get -y update
-sudo apt-get -y upgrade
-sudo apt-get -y install nginx
-sudo mkdir -p /data/web_static/releases/test /data/web_static/shared
-echo "This is a test" | sudo tee /data/web_static/releases/test/index.html
-sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
-sudo chown -hR ubuntu:ubuntu /data/
-sudo sed -i '38i\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n' /etc/nginx/sites-available/default
-sudo service nginx start
+# Install Nginx if not already installed
+if ! [ -x "$(command -v nginx)" ]; then
+    sudo apt-get update
+    sudo apt-get -y install nginx
+fi
+
+# Create the necessary directories
+sudo mkdir -p /data/web_static/{releases/test,shared}
+
+# Create a fake HTML file for testing
+echo "Hello World!" | sudo tee /data/web_static/releases/test/index.html > /dev/null
+
+# Create a symbolic link
+sudo ln -sf /data/web_static/releases/test /data/web_static/current
+
+# Give ownership of the /data/ folder to ubuntu user and group
+sudo chown -R ubuntu:ubuntu /data/
+
+# Update the Nginx configuration
+sudo sed -i '/^\s*server\s*{/a \\\n\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n' /etc/nginx/sites-available/default
+
+# Restart Nginx
+sudo service nginx restart
